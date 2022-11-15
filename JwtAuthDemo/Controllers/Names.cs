1f5b86a2b7b25ac1c98e0c61e6060a141bab0431
Srinivas_Annavarpu <Srinivas.Annavarpu@dellteam.com>
2022-11-15 09:57:17 -0600

    Add project files.

---
+++ JwtAuthDemo/Controllers/Names.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JwtAuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Names : ControllerBase
+    {
+        // GET: api/<Names>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        public IActionResult Authenticate([FromBody]UserCred userCred)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ JwtAuthDemo/JWT/JWTAuthenticationManager.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuthDemo.JWT
+{
+    public class JWTAuthenticationManager : IJWTAuthenticationManager
+    {
+        private readonly string key;
+        private Dictionary<string, string> users = new Dictionary<string, string>()
+        {
+            {"user1", "password1" }, {"user2", "password2"}
+        };
+
+        public JWTAuthenticationManager(string key)
+        {
+            this.key = key;
+        }
+
+        public string Authenticate(string username, string password)
+        {
+            if (!users.Any(u => u.Key == username && u.Value == password))
+                return null;
+
+            var tokenhandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.ASCII.GetBytes(key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(tokenKey),
+                        SecurityAlgorithms.HmacSha256)
+            };
+            var token = tokenhandler.CreateToken(tokenDescriptor);
+            return tokenhandler.WriteToken(token);
+        }
+    }
+}
